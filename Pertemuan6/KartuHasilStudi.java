0aad2ea8d33e8a40a78ea34b80fa055bac37c0d8
Rivalanugrah <rivalanugrahazifebrian16@gmail.com>
2025-04-06 13:21:01 +0700

    first commit

---
+++ Pertemuan6/KartuHasilStudi.java
@@ -0,0 +1,30 @@
+package Pertemuan6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KartuHasilStudi {
+    private String semester;
+    private List<MataKuliah> daftarMatakuliah;
+
+    public KartuHasilStudi(String semester) {
+        this.semester = semester;
+        this.daftarMatakuliah = new ArrayList<>();
+    }
+
+    public void addMatakuliah(MataKuliah mk) {
+        daftarMatakuliah.add(mk);
+    }
+
+    public List<MataKuliah> getDaftarMatakuliah() {
+        return daftarMatakuliah;
+    }
+
+    public String display() {
+        StringBuilder sb = new StringBuilder();
+        for (MataKuliah mk : daftarMatakuliah) {
+            sb.append(mk.display());
+        }
+        return sb.toString();
+    }
+}
