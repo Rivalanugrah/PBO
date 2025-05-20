9f99279bf998311cc51648a4ae5b24141f8f0b9b
Rival Anugrah <147525111+Rivalanugrah@users.noreply.github.com>
2025-05-20 11:52:06 +0700

    Delete Pertemuan11 directory

--- Pertemuan11/Latihan2/Kendaraan.java
+++
@@ -1,7 +0,0 @@
-package Pertemuan11.Latihan2;
-
-public interface Kendaraan {
-    double hitungKonsumsi(double jarak);
-    String getJenis();
-}
-
--- Pertemuan11/Node.java
+++
@@ -1,4 +0,0 @@
-package Pertemuan11;
-
-public class Node {
-}
